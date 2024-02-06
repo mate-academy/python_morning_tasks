78ff031f53c711352668c7c541fc0501ba6af6b3
Oleksandr Parkhomenko <alex.wquels@gmail.com>
2024-02-06 19:31:34 +0200

    reimplement reference solution to utilize Counter, fix namings

--- task_list/python_uncommon_words_from_two_sentences/reference/uncommon_words_from_two_sentences.py
+++ task_list/python_uncommon_words_from_two_sentences/reference/uncommon_words_from_two_sentences.py
@@ -1,9 +1,9 @@
+from collections import Counter
+
+
 def uncommon_sentences(first: str, second: str) -> list[str]:
-    hashmap = {}
-    sentence = f"{first} {second}".split()
-    for word in sentence:
-        if word in hashmap:
-            hashmap[word] += 1
-            continue
-        hashmap[word] = 1
-    return [key for key, value in hashmap.items() if value == 1]
+    return [
+        word
+        for word, appearences in Counter(first.split() + second.split()).items()
+        if appearences == 1
+    ]

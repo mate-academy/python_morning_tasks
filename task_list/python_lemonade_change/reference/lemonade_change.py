345a0e61e03f1343f7373d2c15f98a3d7fe59b3c
Oleksandr Parkhomenko <alex.wquels@gmail.com>
2024-02-07 10:25:07 +0200

    reimplemented pattern matching with if/else approach

--- task_list/python_lemonade_change/reference/lemonade_change.py
+++ task_list/python_lemonade_change/reference/lemonade_change.py
@@ -2,18 +2,16 @@ def sell_lemonade(bills: list[int]) -> bool:
     fives = tens = 0
 
     for bill in bills:
-        pattern = bool(fives), bool(tens), fives >= 3
-        match bill, *pattern:
-            case 5, *_:
-                fives += 1
-            case 10, True, *_:
-                fives -= 1
-                tens += 1
-            case 20, True, True, *_:
-                tens -= 1
-                fives -= 1
-            case 20, *_, True:
-                fives -= 3
-            case _:
-                return False
+        if bill == 5:
+            fives += 1
+        elif bill == 10 and fives:
+            tens += 1
+            fives -= 1
+        elif bill == 20 and fives and tens:
+            tens -= 1
+            fives -= 1
+        elif bill == 20 and fives >= 3:
+            fives -= 3
+        else:
+            return False
     return True

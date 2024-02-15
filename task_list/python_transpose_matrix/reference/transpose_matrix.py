de6f00cfc83a8f2a10bde4a283b1ddf8f2c4bd02
Oleksandr Parkhomenko <alex.wquels@gmail.com>
2024-02-15 18:14:11 +0200

    fix description, fix reference redundant enumerate in reference solution, fix namings in test

--- task_list/python_transpose_matrix/reference/transpose_matrix.py
+++ task_list/python_transpose_matrix/reference/transpose_matrix.py
@@ -2,7 +2,7 @@ def transpose(matrix: list[list[int]]) -> list[list[int]]:
     first_row, *_ = matrix
     result_matrix = [[] for _ in range(len(first_row))]
 
-    for _, row in enumerate(matrix):
+    for row in matrix:
         for elem_idx, elem in enumerate(row):
             result_matrix[elem_idx].append(elem)
     return result_matrix
--- task_list/python_transpose_matrix/transpose_matrix_test.py
+++ task_list/python_transpose_matrix/transpose_matrix_test.py
@@ -2,9 +2,11 @@ from transpose_matrix import transpose
 
 
 def assert_message(
-        matrix: list[list[int]], expected: list[list[int]], output: list[list[int]]
+    matrix: list[list[int]], expected: list[list[int]], output: list[list[int]]
 ) -> list[list[int]]:
-    return f"Failed: Transpose should be {expected} for matrix {matrix}. Output: {output}"
+    return (
+        f"Failed: Transpose should be {expected} for matrix {matrix}. Output: {output}"
+    )
 
 
 def test_basic_cases():
@@ -34,10 +36,21 @@ def test_rectangular_matrix():
 
 
 def test_large_matrix():
-    matrix = [[i * j for j in range(1, 6)] for i in range(1, 6)]
+    rows = range(1, 4)
+    row_elements = range(1, 10)
+    matrix = [
+        [row_num * row_elem_num for row_elem_num in row_elements] for row_num in rows
+    ]
     expected = [
-        [1, 2, 3, 4, 5], [2, 4, 6, 8, 10], [3, 6, 9, 12, 15], 
-        [4, 8, 12, 16, 20], [5, 10, 15, 20, 25]
+        [1, 2, 3],
+        [2, 4, 6],
+        [3, 6, 9],
+        [4, 8, 12],
+        [5, 10, 15],
+        [6, 12, 18],
+        [7, 14, 21],
+        [8, 16, 24],
+        [9, 18, 27],
     ]
     result = transpose(matrix)
     assert result == expected, assert_message(matrix, expected, result)
@@ -56,7 +69,7 @@ def test_with_negative_numbers_in_matrix():
 
 
 def test_large_values():
-    power = 10 ** 9
+    power = 10**9
     matrix = [[power, 2 * power], [3 * power, 4 * power]]
     expected = [[power, 3 * power], [2 * power, 4 * power]]
     result = transpose(matrix)

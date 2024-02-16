c3952feae91be6d2eac33625a5692cf108d1c34a
Oleksandr Parkhomenko <alex.wquels@gmail.com>
2024-02-16 20:09:52 +0200

    remove redundant solution

--- task_list/python_string_matching_in_list/reference/string_matching_in_list.py
+++ task_list/python_string_matching_in_list/reference/string_matching_in_list.py
@@ -1,12 +1,5 @@
 def string_matching(words: list[str]) -> list[str]:
-    def is_substring(first, second):
-        return first in second
+    words_list = " ".join(words)
+    result_list = [word for word in words if words_list.count(word) >= 2]
 
-    words.sort(key=len)
-    result = []
-    for first_idx in range(len(words)):
-        for second_idx in range(first_idx + 1, len(words)):
-            if is_substring(words[first_idx], words[second_idx]):
-                result.append(words[first_idx])
-                break
-    return result
+    return result_list
--- task_list/python_string_matching_in_list/reference/string_matching_in_list_second_solution.py
+++
@@ -1,5 +0,0 @@
-def string_matching(words: list[str]) -> list[str]:
-    words_list = " ".join(words)
-    result_list = [word for word in words if words_list.count(word) >= 2]
-
-    return result_list

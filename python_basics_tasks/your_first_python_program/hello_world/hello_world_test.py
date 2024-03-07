03be6b7d688c5d5a4a325e2b45f1d4467e8f13d3
Oleksandr Parkhomenko <alex.wquels@gmail.com>
2024-03-07 16:44:19 +0200

    Implemented [hello world](python_basics:your_first_python_program)

---
+++ python_basics_tasks/your_first_python_program/hello_world/hello_world_test.py
@@ -0,0 +1,18 @@
+import sys
+
+from io import StringIO
+
+
+def test_hello_world_printed():
+    expected_output = "Hello, world!"
+
+    captured_output = StringIO()
+    sys.stdout = captured_output
+
+    exec(open("hello_world.py").read())
+    printed_output = captured_output.getvalue().strip()
+    sys.stdout = sys.__stdout__
+
+    assert (
+        printed_output == expected_output
+    ), f"Failed. Printed phrase is not valid. Printed: {printed_output}, while Expected: {expected_output}" 

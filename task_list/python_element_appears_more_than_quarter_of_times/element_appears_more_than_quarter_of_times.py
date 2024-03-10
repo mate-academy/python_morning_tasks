7e826c1e4e89ed7d5a409a0f78917e95490c5b6f
Oleksandr Parkhomenko <alex.wquels@gmail.com>
2024-03-10 23:37:17 +0200

    removed reference solution from boilerplate file

--- task_list/python_element_appears_more_than_quarter_of_times/element_appears_more_than_quarter_of_times.py
+++ task_list/python_element_appears_more_than_quarter_of_times/element_appears_more_than_quarter_of_times.py
@@ -1,18 +1,2 @@
-# def find_integer(num_list: list[int]) -> int:
-#     pass
 def find_integer(num_list: list[int]) -> int:
-    if len(num_list) <= 2:
-        return num_list[0]
-
-    target = len(num_list) // 4
-    result_mapper = {}
-
-    for num in num_list:
-        if num in result_mapper:
-            result_mapper[num] += 1
-            if result_mapper.get(num) > target:
-                return num
-            continue
-        result_mapper[num] = 1
-
-    return max(result_mapper, key=result_mapper.get)
+    pass

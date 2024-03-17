7582a22ff0f89ed0c2eb35bba69d3395164ae8ad
Oleksandr Parkhomenko <alex.wquels@gmail.com>
2024-03-17 12:50:32 +0200

    add tests for interpolation, fix tests

--- python_basics_tasks/strings/python_greeting_inter/greeting_inter_test.py
+++ python_basics_tasks/strings/python_greeting_inter/greeting_inter_test.py
@@ -1,35 +1,36 @@
+import re
 import subprocess
 
 import greeting_inter
 
 
-VARIABLES = greeting_inter
+TESTED_FILE = greeting_inter
 FIRST_NAME_VAR = "first_name"
 RESULT_VAR = "greeting"
 
 
 def test_all_variables_defined():
     assert hasattr(
-        VARIABLES, FIRST_NAME_VAR
-    ), f"Failed. Variable {FIRST_NAME_VAR} should be defined"
+        TESTED_FILE, FIRST_NAME_VAR
+    ), f"Failed. Variable '{FIRST_NAME_VAR}' should be defined"
     assert hasattr(
-        VARIABLES, RESULT_VAR
-    ), f"Failed. Variable {RESULT_VAR} should be defined"
+        TESTED_FILE, RESULT_VAR
+    ), f"Failed. Variable '{RESULT_VAR}' should be defined"
 
-    first_name = getattr(VARIABLES, FIRST_NAME_VAR)
-    greeting = getattr(VARIABLES, RESULT_VAR)
+    first_name = getattr(TESTED_FILE, FIRST_NAME_VAR)
+    greeting = getattr(TESTED_FILE, RESULT_VAR)
 
     assert isinstance(
         first_name, str
-    ), f"Failed. Variable {FIRST_NAME_VAR} should be <str> type"
+    ), f"Failed. Variable '{FIRST_NAME_VAR}' should be <str> type"
     assert isinstance(
         greeting, str
-    ), f"Failed. Variable {RESULT_VAR} should be <str> type"
+    ), f"Failed. Variable '{RESULT_VAR}' should be <str> type"
 
     expected = f"Hello, {first_name}!"
     assert (
         greeting == expected
-    ), f"Failed. Variable {RESULT_VAR} should contain {expected} string. {greeting} != {expected}"
+    ), f"Failed. Variable '{RESULT_VAR}' should contain {expected} string. {greeting} != {expected}"
 
 
 def test_greeting_was_printed():
@@ -38,9 +39,31 @@ def test_greeting_was_printed():
     captured_output = process.communicate()[0].decode()
     printed_output = captured_output.strip()
 
-    first_name = getattr(VARIABLES, FIRST_NAME_VAR)
+    first_name = getattr(TESTED_FILE, FIRST_NAME_VAR)
     expected = f"Hello, {first_name}!"
 
     assert (
         printed_output == expected
-    ), f"Failed. {RESULT_VAR} should be printed out. {printed_output} != {expected}"
+    ), f"Failed. '{RESULT_VAR}' should be printed out. {printed_output} != {expected}"
+
+
+def test_greeting_should_not_be_hardcoded():
+    first_name = getattr(TESTED_FILE, FIRST_NAME_VAR)
+
+    with open(TESTED_FILE.__file__) as f:
+        code = f.read()
+
+        assignment_regex = f"['\"]Hello, {first_name}!['\"]"
+        assert (
+            re.search(assignment_regex, code) is None
+        ), f"Variable '{RESULT_VAR}' should be assigned value using interpolation"
+
+
+def test_greeting_should_not_be_assigned_using_concatenation():
+    with open(TESTED_FILE.__file__) as f:
+        code = f.read()
+
+        assignment_regex = r"\+"
+        assert (
+            re.search(assignment_regex, code) is None
+        ), "Variable '{RESULT_VAR}' should be assigned value using interpolation"

fca7b5358dc3e4771b7e01e5a4a7f0d149425ac5
Alex Zhidkov <alex.z@mate.academy>
2024-03-25 10:48:19 +0200

    Update python_basics_tasks/boolean/python_turn_on_the_light/turn_on_the_light_test.py

--- python_basics_tasks/boolean/python_turn_on_the_light/turn_on_the_light_test.py
+++ python_basics_tasks/boolean/python_turn_on_the_light/turn_on_the_light_test.py
@@ -51,7 +51,7 @@ def test_result_not_hardcoded():
 
 def test_result_printed_with_not():
     with open(TESTED_FILE.__file__) as file:
-        code = f.read()
+        code = file.read()
 
         pattern = r"print\(not is_light_on\)"
         assert (

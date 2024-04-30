e6aa667ee768f00d407ac5d419611598b11a022e
Alex Zhidkov <alex.z@mate.academy>
2024-04-30 10:03:57 +0300

    Update python_basics_tasks/conditional_operators/python_eligibility_for_voting_2/eligibility_for_voting_2_test.py

--- python_basics_tasks/conditional_operators/python_eligibility_for_voting_2/eligibility_for_voting_2_test.py
+++ python_basics_tasks/conditional_operators/python_eligibility_for_voting_2/eligibility_for_voting_2_test.py
@@ -12,14 +12,14 @@ PHRASE_TO_PRINT = "You cannot vote"
 def test_variable_is_properly_defined():
     assert hasattr(
         TESTED_FILE, VAR_NAME
-    ), f"Failed. Variable {VAR_NAME} should be defined"
+    ), f"Failed. Variable '{VAR_NAME}' should be defined."
 
     age = getattr(TESTED_FILE, VAR_NAME)
 
-    assert isinstance(age, int), f"Failed. {VAR_NAME} should be type <int>"
+    assert isinstance(age, int), f"Failed. '{VAR_NAME}' should be type <int>."
     assert (
         age == VAR_VALUE
-    ), f"Failed. {VAR_NAME} should be equal to <{VAR_VALUE}>. {VAR_VALUE} != {age}"
+    ), f"Failed. '{VAR_NAME}' should be equal to <{VAR_VALUE}>. '{VAR_VALUE} != {age}'."
 
 
 def test_print_works_correctly():
@@ -32,4 +32,4 @@ def test_print_works_correctly():
 
     assert (
         captured_print == PHRASE_TO_PRINT
-    ), f"Failed. {PHRASE_TO_PRINT} should be printed. Your output: {captured_print}"
+    ), f"Failed. {PHRASE_TO_PRINT} should be printed. Your output: '{captured_print}'"
